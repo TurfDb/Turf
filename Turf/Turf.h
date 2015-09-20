8b656a8736047bd0043d60e772a87392568d8229
Jordan Hamill <jordan@shopkeep.com>
2015-09-20 20:58:04 +0100

    Initial Commit

---
+++ Turf/Turf.h
@@ -0,0 +1,19 @@
+//
+//  Turf.h
+//  Turf
+//
+//  Created by Jordan Hamill on 20/09/2015.
+//  Copyright © 2015 JordanHamill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Turf.
+FOUNDATION_EXPORT double TurfVersionNumber;
+
+//! Project version string for Turf.
+FOUNDATION_EXPORT const unsigned char TurfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Turf/PublicHeader.h>
+
+
