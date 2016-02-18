3c1daf0fb77ee4c3ae7faaddb7234182cc090cb1
Jordan Hamill <jordan@shopkeep.com>
2016-02-18 22:43:33 +0000

    Get sqlite3 working with Xcode7.3

--- Turf/Turf.h
+++ Turf/Turf.h
@@ -14,6 +14,4 @@ FOUNDATION_EXPORT double TurfVersionNumber;
 //! Project version string for Turf.
 FOUNDATION_EXPORT const unsigned char TurfVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Turf/PublicHeader.h>
-
-
+#import "sqlite3.h"
