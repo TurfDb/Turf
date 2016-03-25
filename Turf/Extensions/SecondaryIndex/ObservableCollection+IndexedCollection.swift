c309443c7fff749449d295e5bae98333222aef85
Jordan Hamill <jordan@shopkeep.com>
2016-03-25 18:27:49 +0000

    Allow observables to use where clauses and prepared queries

--- Turf/Extensions/SecondaryIndex/ObservableCollection+IndexedCollection.swift
+++ Turf/Extensions/SecondaryIndex/ObservableCollection+IndexedCollection.swift
@@ -8,6 +8,70 @@ extension ObservableCollection where TCollection: IndexedCollection {
      If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
      - note:
      - Thread safe.
+     - parameter clause: Secondary indexed query to execute on collection change.
+     - parameter thread: Thread to execute the prefilter and potential query on.
+     - parameter prefilterChangeSet: Executed before querying the collection to determine if the query is required.
+     */
+    public func valuesWhere(clause: WhereClause, thread: CallbackThread = .CallingThread, prefilterChangeSet: Prefilter = nil) -> CollectionTypeObserver<[TCollection.Value]> {
+        let queryResultsObserver = CollectionTypeObserver<[TCollection.Value]>(initalValue: [])
+
+        let disposable =
+            didChange(thread) { (collection, changeSet) in
+                let canCheckPreviousValue = prefilterChangeSet != nil && queryResultsObserver.value.count > 0
+                let shouldRequery = canCheckPreviousValue ? prefilterChangeSet!(queryResultsObserver.value, changeSet) : true
+
+                if shouldRequery {
+                    let queryResults = collection!.findValuesWhere(clause)
+                    queryResultsObserver.setValue(queryResults, fromTransaction: collection!.readTransaction)
+                }
+        }
+
+        queryResultsObserver.disposeBag.add(disposable)
+        // If disposing ancestors, dispose this collection and all its child observers by removing from ObservingConnection
+        queryResultsObserver.disposeBag.parent = self.disposeBag
+
+        return queryResultsObserver
+    }
+
+    // MARK: Prepared query
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter preparedQuery: Prepared secondary indexed query to execute on collection change.
+     - parameter thread: Thread to execute the prefilter and potential query on.
+     - parameter prefilterChangeSet: Executed before querying the collection to determine if the query is required.
+     */
+    public func valuesWhere(preparedQuery: PreparedValuesWhereQuery, thread: CallbackThread = .CallingThread, prefilterChangeSet: Prefilter = nil) -> CollectionTypeObserver<[TCollection.Value]> {
+        let queryResultsObserver = CollectionTypeObserver<[TCollection.Value]>(initalValue: [])
+
+        let disposable =
+            didChange(thread) { (collection, changeSet) in
+                let canCheckPreviousValue = prefilterChangeSet != nil && queryResultsObserver.value.count > 0
+                let shouldRequery = canCheckPreviousValue ? prefilterChangeSet!(queryResultsObserver.value, changeSet) : true
+
+                if shouldRequery {
+                    let queryResults = collection!.findValuesWhere(preparedQuery)
+                    queryResultsObserver.setValue(queryResults, fromTransaction: collection!.readTransaction)
+                }
+        }
+
+        queryResultsObserver.disposeBag.add(disposable)
+        // If disposing ancestors, dispose this collection and all its child observers by removing from ObservingConnection
+        queryResultsObserver.disposeBag.parent = self.disposeBag
+
+        return queryResultsObserver
+    }
+
+    // MARK: Raw SQL query
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
      - parameter predicate: Secondary indexed query to execute on collection change.
      - parameter thread: Thread to execute the prefilter and potential query on.
      - parameter prefilterChangeSet: Executed before querying the collection to determine if the query is required.
