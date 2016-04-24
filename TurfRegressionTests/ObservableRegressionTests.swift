395f1a25abe222c6a4e486df2d9959f9b3ddfab1
Jordan Hamill <jordan@shopkeep.com>
2016-04-24 14:18:33 +0100

    Observable regression tests

--- TurfRegressionTests/ObservableRegressionTests.swift
+++ TurfRegressionTests/ObservableRegressionTests.swift
@@ -1,9 +1,161 @@
-//
-//  ObservableRegressionTests.swift
-//  Turf
-//
-//  Created by Jordan Hamill on 28/03/2016.
-//  Copyright © 2016 JordanHamill. All rights reserved.
-//
+import Quick
+import Nimble
+import Turf
 
-import Foundation
+class ObservablesRegressionTests: QuickSpec {
+    override func spec() {
+        describe("Observables regression tests") {
+            var tester: TestDatabase!
+            beforeEach {
+                tester = try! TestDatabase(databasePath: "ObservableRegressionTests.sqlite")
+                try! tester.connection1.readWriteTransaction{ transaction, collections in
+                    transaction.readWrite(collections.cars).removeAllValues()
+                    transaction.readWrite(collections.wheels).removeAllValues()
+                }
+            }
+
+            context("when observing the cars collection") {
+                var observableCarsCollection: ObservableCollection<CarsCollection, Collections>!
+                beforeEach {
+                    observableCarsCollection = tester.observingConnection.observeCollection(tester.collections.cars)
+                }
+
+                context("and a value is written to the wheels collection") {
+                    it("does not call didChange") {
+
+                        var didChangeCalled = false
+                        observableCarsCollection.didChange { carsCollection, changes in
+                            didChangeCalled = true
+                        }
+
+                        try! tester.connection1.readWriteTransaction { transaction, collections in
+                            let wheel = WheelModel(uuid: "test_1", manufacturer: "Pirelli", size: 18.0)
+                            transaction.readWrite(collections.wheels)
+                                .setValue(wheel, forKey: wheel.uuid)
+                        }
+
+                        expect(didChangeCalled) == false
+                    }
+                }
+
+                context("and a value is written to the cars collection") {
+                    it("calls didChange with only an insert for test_1") {
+
+                        var changeSet: ChangeSet<String>?
+                        observableCarsCollection.didChange { carsCollection, changes in
+                            changeSet = changes
+                        }
+
+                        try! tester.connection1.readWriteTransaction { transaction, collections in
+                            let car = CarModel(uuid: "test_1", manufacturer: "McLaren", name: "P1", doors: 2)
+                            transaction.readWrite(collections.cars)
+                                .setValue(car, forKey: car.uuid)
+                        }
+
+                        expect(changeSet?.hasChangeForKey("test_1")) == true
+                        expect(changeSet?.changes.count) == 1
+                        expect(changeSet?.allValuesRemoved) == false
+
+                        switch changeSet!.changes.first! {
+                        case .Insert(let key):
+                            if key != "test_1" { fail() }
+                        default: fail()
+                        }
+                    }
+
+                    context("and the value is updated") {
+                        beforeEach {
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                let car = CarModel(uuid: "test_1", manufacturer: "McLaren", name: "P1", doors: 2)
+                                transaction.readWrite(collections.cars)
+                                    .setValue(car, forKey: car.uuid)
+                            }
+                        }
+
+                        it("calls didChange with only an update for test_1") {
+
+                            var changeSet: ChangeSet<String>?
+                            observableCarsCollection.didChange { carsCollection, changes in
+                                changeSet = changes
+                            }
+
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                let car = CarModel(uuid: "test_1", manufacturer: "McLaren", name: "P1", doors: 2)
+                                transaction.readWrite(collections.cars)
+                                    .setValue(car, forKey: car.uuid)
+                            }
+
+                            expect(changeSet?.hasChangeForKey("test_1")) == true
+                            expect(changeSet?.changes.count) == 1
+                            expect(changeSet?.allValuesRemoved) == false
+
+                            switch changeSet!.changes.first! {
+                            case .Update(let key):
+                                if key != "test_1" { fail() }
+                            default: fail()
+                            }
+                        }
+                    }
+
+                    context("and the value is removed") {
+                        beforeEach {
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                let car = CarModel(uuid: "test_1", manufacturer: "McLaren", name: "P1", doors: 2)
+                                transaction.readWrite(collections.cars)
+                                    .setValue(car, forKey: car.uuid)
+                            }
+                        }
+
+                        it("calls didChange with only a remove for test_1") {
+
+                            var changeSet: ChangeSet<String>?
+                            observableCarsCollection.didChange { carsCollection, changes in
+                                changeSet = changes
+                            }
+
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                transaction.readWrite(collections.cars).removeValuesWithKeys(["test_1"])
+                            }
+
+                            expect(changeSet?.hasChangeForKey("test_1")) == true
+                            expect(changeSet?.changes.count) == 1
+                            expect(changeSet?.allValuesRemoved) == false
+
+                            switch changeSet!.changes.first! {
+                            case .Remove(let key):
+                                if key != "test_1" { fail() }
+                            default: fail()
+                            }
+                        }
+                    }
+
+                    context("and all values are removed from cars") {
+                        beforeEach {
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                let car = CarModel(uuid: "test_1", manufacturer: "McLaren", name: "P1", doors: 2)
+                                transaction.readWrite(collections.cars)
+                                    .setValue(car, forKey: car.uuid)
+                            }
+                        }
+
+                        it("calls didChange with only allValuesRemoved set") {
+
+                            var changeSet: ChangeSet<String>?
+                            observableCarsCollection.didChange { carsCollection, changes in
+                                changeSet = changes
+                            }
+
+                            try! tester.connection1.readWriteTransaction { transaction, collections in
+                                transaction.readWrite(collections.cars).removeAllValues()
+                            }
+
+                            expect(changeSet?.hasChangeForKey("test_1")) == true
+                            expect(changeSet?.changes.count) == 0
+                            expect(changeSet?.allValuesRemoved) == true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
