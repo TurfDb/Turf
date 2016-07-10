8aef6d5529d29e5bc9baedd898a00a9bc893c225
Jordan Hamill <jordan@shopkeep.com>
2016-07-10 20:19:47 +0100

    Restore various observable queries

--- Turf/Extensions/SecondaryIndex/ObservableCollection+IndexedCollection.swift
+++ Turf/Extensions/SecondaryIndex/ObservableCollection+IndexedCollection.swift
@@ -6,28 +6,11 @@ extension ObservableCollection where TCollection: IndexedCollection {
     // MARK: Public methods
 
     /**
-     Observe the values returned by `predicate` after a collection change.
-     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     Observe the values returned by `predicate` after every collection change.
      - note:
      - Thread safe.
-     - parameter clause: Secondary indexed query to execute on collection change.
-     - parameter thread: Thread to execute the prefilter and potential query on.
-     - parameter prefilterChangeSet: Executed before querying the collection to determine if the query is required.
+     - parameter matching: Secondary indexed query to execute on collection change.
      */
-    public func values(matching clause: WhereClause, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
-        var previous: [TCollection.Value] = []
-
-        return self.filterChangeSet { (changeSet) -> Bool in
-                return prefilter(changeSet: changeSet, previousValues: previous)
-            }.map { (collection, changeSet)  in
-                let newValues = collection.findValuesWhere(clause)
-                previous = newValues
-                return TransactionalValue(transaction: collection.readTransaction, value: newValues)
-            }
-    }
-
-    //TODO PreparedValuesWhereQuery - could it conform to a protocol to make these generic or would that make the signature unwieldy and slow
-
     public func values(matching clause: WhereClause) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
         return self.map { (collection, changeSet)  in
             let newValues = collection.findValuesWhere(clause)
@@ -35,10 +18,44 @@ extension ObservableCollection where TCollection: IndexedCollection {
         }
     }
 
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
+    public func values(matching clause: WhereClause, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
+        var previous: [TCollection.Value] = []
+
+        return self.filterChangeSet { (changeSet) -> Bool in
+            return prefilter(changeSet: changeSet, previousValues: previous)
+            }.map { (collection, changeSet)  in
+                let newValues = collection.findValuesWhere(clause)
+                previous = newValues
+                return TransactionalValue(transaction: collection.readTransaction, value: newValues)
+        }
+    }
+
+    /**
+     Observe the values returned by `predicate` after every collection change.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     */
     public func indexableValues(matching clause: WhereClause) -> IndexableObservable<[TCollection.Value]> {
         return IndexableObservable(observable: self.map { return $0.0.findValuesWhere(clause) })
     }
 
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
     public func indexableValues(matching clause: WhereClause, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> IndexableObservable<[TCollection.Value]> {
         var previous: [TCollection.Value] = []
 
@@ -54,7 +71,133 @@ extension ObservableCollection where TCollection: IndexedCollection {
 
     // MARK: Prepared query
 
+    /**
+     Observe the values returned by `predicate` after every collection change.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     */
+    public func values(matching clause: PreparedValuesWhereQuery<Collections>) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
+        return self.map { (collection, changeSet)  in
+            let newValues = collection.findValuesWhere(clause)
+            return TransactionalValue(transaction: collection.readTransaction, value: newValues)
+        }
+    }
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
+    public func values(matching clause: PreparedValuesWhereQuery<Collections>, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
+        var previous: [TCollection.Value] = []
+
+        return self.filterChangeSet { (changeSet) -> Bool in
+            return prefilter(changeSet: changeSet, previousValues: previous)
+            }.map { (collection, changeSet)  in
+                let newValues = collection.findValuesWhere(clause)
+                previous = newValues
+                return TransactionalValue(transaction: collection.readTransaction, value: newValues)
+        }
+    }
+
+    /**
+     Observe the values returned by `predicate` after every collection change.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     */
+    public func indexableValues(matching clause: PreparedValuesWhereQuery<Collections>) -> IndexableObservable<[TCollection.Value]> {
+        return IndexableObservable(observable: self.map { return $0.0.findValuesWhere(clause) })
+    }
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matching: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
+    public func indexableValues(matching clause: PreparedValuesWhereQuery<Collections>, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> IndexableObservable<[TCollection.Value]> {
+        var previous: [TCollection.Value] = []
+
+        let observable = self.filterChangeSet { (changeSet) -> Bool in
+            return prefilter(changeSet: changeSet, previousValues: previous)
+            }.map { (collection, changeSet) -> [TCollection.Value] in
+                let newValues = collection.findValuesWhere(clause)
+                previous = newValues
+                return newValues
+        }
+        return IndexableObservable(observable: observable)
+    }
 
     // MARK: Raw SQL query
 
+    /**
+     Observe the values returned by `predicate` after every collection change.
+     - note:
+     - Thread safe.
+     - parameter matchingRawSql: Secondary indexed query to execute on collection change.
+     */
+    public func values(matchingRawSql clause: String) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
+        return self.map { (collection, changeSet)  in
+            let newValues = collection.findValuesWhere(clause)
+            return TransactionalValue(transaction: collection.readTransaction, value: newValues)
+        }
+    }
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matchingRawSql: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
+    public func values(matchingRawSql clause: String, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> Observable<TransactionalValue<[TCollection.Value], Collections>> {
+        var previous: [TCollection.Value] = []
+
+        return self.filterChangeSet { (changeSet) -> Bool in
+            return prefilter(changeSet: changeSet, previousValues: previous)
+            }.map { (collection, changeSet)  in
+                let newValues = collection.findValuesWhere(clause)
+                previous = newValues
+                return TransactionalValue(transaction: collection.readTransaction, value: newValues)
+        }
+    }
+
+    /**
+     Observe the values returned by `predicate` after every collection change.
+     - note:
+     - Thread safe.
+     - parameter matchingRawSql: Secondary indexed query to execute on collection change.
+     */
+    public func indexableValues(matchingRawSql clause: String) -> IndexableObservable<[TCollection.Value]> {
+        return IndexableObservable(observable: self.map { return $0.0.findValuesWhere(clause) })
+    }
+
+    /**
+     Observe the values returned by `predicate` after a collection change.
+     If the query is expensive, the collection change set can be examined first by using `prefilterChangeSet`.
+     - note:
+     - Thread safe.
+     - parameter matchingRawSql: Secondary indexed query to execute on collection change.
+     - parameter prefilter: Executed before querying the collection to determine if the query is required.
+     */
+    public func indexableValues(matchingRawSql clause: String, prefilter: (changeSet: ChangeSet<String>, previousValues: [TCollection.Value]) -> Bool) -> IndexableObservable<[TCollection.Value]> {
+        var previous: [TCollection.Value] = []
+
+        let observable = self.filterChangeSet { (changeSet) -> Bool in
+            return prefilter(changeSet: changeSet, previousValues: previous)
+            }.map { (collection, changeSet) -> [TCollection.Value] in
+                let newValues = collection.findValuesWhere(clause)
+                previous = newValues
+                return newValues
+        }
+        return IndexableObservable(observable: observable)
+    }
 }
