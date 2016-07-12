d5ccbebbcd8107a9830bda5c856794375423f87a
Jordan Hamill <jordan@shopkeep.com>
2016-07-12 23:43:13 +0100

    Add map

--- Turf/Observable/TransactionalValue.swift
+++ Turf/Observable/TransactionalValue.swift
@@ -8,3 +8,14 @@ public struct TransactionalValue<Value, Collections: CollectionsContainer> {
     public unowned let transaction: ReadTransaction<Collections>
     public let value: Value
 }
+
+extension TransactionalValue where Value: SequenceType {
+    public func map<Mapped>(map: (Value) -> Mapped) -> TransactionalValue<Mapped, Collections> {
+        return TransactionalValue<Mapped, Collections>(transaction: transaction, value: map(value))
+    }
+
+    public func map<MappedElement>(map: (Value.Generator.Element) -> MappedElement) -> TransactionalValue<[MappedElement], Collections> {
+        let mappedValues = value.map(map)
+        return TransactionalValue<[MappedElement], Collections>(transaction: transaction, value: mappedValues)
+    }
+}
